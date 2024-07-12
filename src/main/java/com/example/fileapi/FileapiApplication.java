139fb03d6a532b9ef93f22d72356eb283d0884c2
disnaimov <dimitriy.kiselev0@mail.ru>
2024-07-12 13:00:20 +0400

    Init

---
+++ src/main/java/com/example/fileapi/FileapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileapiApplication.class, args);
+	}
+
+}
